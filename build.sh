f3e6d0016c6bc6f748c0b6c8722e3e38c7daf714
sfan5 <sfan5@live.de>
2022-12-17 12:32:17 +0100

    enable dbengine

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -e
 
 if command -v apt-get; then
-	sudo apt-get install gcc g++ make libuv1-dev zlib1g-dev pkg-config uuid-dev autoconf automake
+	sudo apt-get install gcc g++ make libuv1-dev zlib1g-dev pkg-config \
+		uuid-dev liblz4-dev libssl-dev autoconf automake
 fi
 
 [ -d netdata ] || git clone https://github.com/sfan5/netdata-custom --depth=1 -b netdata netdata
 pushd netdata
 
 autoreconf -ivf
-LDFLAGS="-s" ./configure --prefix=/opt/netdata --with-user=netdata
+LDFLAGS="-s" ./configure --prefix=/opt/netdata --with-user=netdata \
+	--enable-dbengine
 nice make -j4
 
 make DESTDIR="$PWD/_i" install
