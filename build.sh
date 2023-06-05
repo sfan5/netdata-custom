6d9c1c27420dbd04a521a8adea259420f4b4e438
sfan5 <sfan5@live.de>
2023-06-05 14:06:59 +0200

    Add install line for yum

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
 if command -v apt-get; then
-	sudo apt-get install gcc g++ make libuv1-dev zlib1g-dev pkg-config \
+	sudo apt-get install gcc g++ git-core make libuv1-dev zlib1g-dev pkg-config \
 		uuid-dev liblz4-dev libssl-dev autoconf automake
+elif command -v yum; then
+	sudo yum install gcc gcc-c++ git-core make libuv-devel zlib-devel pkg-config \
+		libuuid-devel lz4-devel openssl-devel autoconf automake
 fi
 
 [ -d netdata ] || git clone https://github.com/sfan5/netdata-custom --depth=1 -b netdata netdata
